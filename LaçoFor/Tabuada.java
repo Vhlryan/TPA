8425f0b18cef7d028cb97c725575ba052af8c298
Vhlryan <ryanalvesbatista178@gmail.com>
2025-11-24 18:23:17 -0300

    Add files via upload

---
+++ LaçoFor/Tabuada.java
@@ -0,0 +1,21 @@
+package lacofor;
+
+public class Tabuada {
+	
+	public static void main (String[] args) { 
+
+	
+	
+for(int i=1; i <= 10 ; i=i+1) {
+	
+		for(int n=1; n <= 10 ; n=n+1) {
+			
+		System.out.println(i + " x " + n + " = " + i*n);
+		}
+		
+	}
+
+}
+}
+	
+
